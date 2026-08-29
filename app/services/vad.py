2cdfe04e0317d4d91a27ec8f2b10b2c3ed22b096
Sakshith <shettysakshith3@gmail.com>
2026-08-29 23:53:08 +0530

    commit app.js file

---
+++ app/services/vad.py
@@ -0,0 +1,6 @@
+import vad
+import torch
+
+class VADService:
+    def __init__(self,sample_rate: init =  16000):
+        pass
--- static/app.js
+++ static/app.js
@@ -3,5 +3,30 @@ let mediaRecorder;
 
 async function startRecording(){
     const log = document.getElementById('log');
-    socket = new WebSocket(`ws://${window.location.host}ws/audio`);
+    socket = new WebSocket(`ws://${window.location.host}/ws/audio`);
+
+    socket.onopen = async () => {
+        log.innerText = "Status : Connected. Listening....";
+        const stream = await navigator.mediaDevices.getUserMedia({audio:true});
+        mediaRecorder = new MediaRecorder(stream , {mimeType:'audio/webm'});
+
+        mediaRecorder.ondataavailable = (event) => {
+            if (event.data.size > 0 && socket.readyState === WebSocket.OPEN){
+                socket.send(event.data);
+            }
+        };
+        mediaRecorder.start(100);
+    };
+    socket.onmessage = (event) => {
+        console.log("Server response:", event(data));
+    };
+
+    socket.onclose = () => {
+        log.innerText = "status: Disconnected"
+    };
+}
+
+function stopRecording() {
+    if (mediaRecorder) mediaRecorder.stop();
+    if (socket) socket.close();
 }
