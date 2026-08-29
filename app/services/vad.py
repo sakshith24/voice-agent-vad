996c414a1320baa323d300d410ad3712cb25bb21
Sakshith <shettysakshith3@gmail.com>
2026-08-29 23:57:06 +0530

    commit vad file

--- app/services/vad.py
+++ app/services/vad.py
@@ -2,5 +2,13 @@ import vad
 import torch
 
 class VADService:
-    def __init__(self,sample_rate: init =  16000):
-        pass
+    def __init__(self,sample_rate: int =  16000, thresshold: float = 0.5):
+        self.sample_rate = sample_rate
+        self.thresshold = thresshold
+        self.model, self.utils = torch.hub.load(
+            repo_or_dir='snakers4/silero-vad',
+            model='silero_vad',
+            force_reload=False,
+            onnx=False
+        )
+        (self.get_speech_timestamps, _, _, _, _) = self.utils
